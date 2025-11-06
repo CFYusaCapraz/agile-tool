5b6b21bb002997cb8f351ae6c7a4309d92f1c097
Yuşa Çapraz <yusacapraz@protonmail.com>
2025-11-07 01:01:59 +0300

    Initial commit

---
+++ src/main/java/com/cfyusacapraz/agiletool/AgileToolApplication.java
@@ -0,0 +1,13 @@
+package com.cfyusacapraz.agiletool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cfyusacapraz/agiletool/AgileToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfyusacapraz.agiletool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
