fae4c88945460682e12d5d1956f69e01a25a7c1a
Yuşa Çapraz <yusacapraz@protonmail.com>
2025-11-08 05:05:06 +0300

    feat: add JPA configuration class for transaction management and repository scanning

---
+++ src/main/java/com/cfyusacapraz/agiletool/config/JpaConfig.java
@@ -0,0 +1,24 @@
+package com.cfyusacapraz.agiletool.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.cfyusacapraz.agiletool.repository")
+@EntityScan(basePackages = "com.cfyusacapraz.agiletool.domain")
+@EnableTransactionManagement
+class JpaConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
+}
