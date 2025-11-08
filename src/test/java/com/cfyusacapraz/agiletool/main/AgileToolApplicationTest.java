570f4ddcc067edef1a8b86082f7d7a9b04eb55cb
Yuşa Çapraz <yusacapraz@protonmail.com>
2025-11-08 18:15:56 +0300

    feat: add initial test class for AgileTool application context loading

---
+++ src/test/java/com/cfyusacapraz/agiletool/main/AgileToolApplicationTest.java
@@ -0,0 +1,13 @@
+package com.cfyusacapraz.agiletool.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
