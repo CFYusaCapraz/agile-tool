fb05706ddc0dfa4a265d6f0453fed1f306ec2beb
Yuşa Çapraz <yusacapraz@protonmail.com>
2025-11-18 13:27:44 +0300

    feat: remove CompletableFuture and asynchronous handling from UserController

--- src/main/java/com/cfyusacapraz/agiletool/api/controller/UserController.java
+++ src/main/java/com/cfyusacapraz/agiletool/api/controller/UserController.java
@@ -5,18 +5,16 @@ import com.cfyusacapraz.agiletool.api.request.UserCreateRequest;
 import com.cfyusacapraz.agiletool.api.request.UserFilterRequest;
 import com.cfyusacapraz.agiletool.api.request.UserUpdateRequest;
 import com.cfyusacapraz.agiletool.api.response.UserResponse;
-import com.cfyusacapraz.agiletool.api.response.base.BaseApiResponse;
-import com.cfyusacapraz.agiletool.api.response.base.PagedListResultResponse;
-import com.cfyusacapraz.agiletool.api.response.base.SaveEntityResponse;
-import com.cfyusacapraz.agiletool.api.response.base.SingleResultResponse;
+import com.cfyusacapraz.agiletool.api.response.base.*;
+import com.cfyusacapraz.agiletool.dto.UserDto;
 import com.cfyusacapraz.agiletool.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Pair;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(path = ApiEndpoints.USER_BASE_URL, produces = ApiEndpoints.RESPONSE_CONTENT_TYPE)
@@ -27,35 +25,34 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public CompletableFuture<SaveEntityResponse<UUID>> createUser(@Validated @RequestBody UserCreateRequest userCreateRequest) {
-        return userService.create(userCreateRequest)
-                .thenApply(SaveEntityResponse::new);
+    public SaveEntityResponse<UUID> createUser(@Validated @RequestBody UserCreateRequest userCreateRequest) {
+        UserDto userDto = userService.create(userCreateRequest);
+        return new SaveEntityResponse<>(userDto);
     }
 
     @PutMapping(path = "/{userId}")
-    public CompletableFuture<SaveEntityResponse<UUID>> updateUser(@PathVariable("userId") UUID id, @Validated @RequestBody UserUpdateRequest userUpdateRequest) {
-        return userService.update(id, userUpdateRequest)
-                .thenApply(SaveEntityResponse::new);
+    public SaveEntityResponse<UUID> updateUser(@PathVariable("userId") UUID id,
+                                               @Validated @RequestBody UserUpdateRequest userUpdateRequest) {
+        UserDto userDto = userService.update(id, userUpdateRequest);
+        return new SaveEntityResponse<>(userDto);
     }
 
     @DeleteMapping(path = "/{userId}")
-    public CompletableFuture<BaseApiResponse> deleteUser(@PathVariable("userId") UUID id) {
-        return userService.delete(id)
-                .thenApply(aVoid -> new BaseApiResponse(null, true));
+    public BaseApiResponse deleteUser(@PathVariable("userId") UUID id) {
+        userService.delete(id);
+        return new BaseApiResponse(null, true);
     }
 
     @GetMapping(path = "/{userId}")
-    public CompletableFuture<SingleResultResponse<UserResponse>> getUser(@PathVariable("userId") UUID id) {
-        return userService.getById(id)
-                .thenApply(userDto -> new SingleResultResponse<>(new UserResponse(userDto)));
+    public SingleResultResponse<UserResponse> getUser(@PathVariable("userId") UUID id) {
+        UserDto userDto = userService.getById(id);
+        return new SingleResultResponse<>(new UserResponse(userDto));
     }
 
     @GetMapping
-    public CompletableFuture<PagedListResultResponse<List<UserResponse>>> getAllUsers(@Validated UserFilterRequest userFilterRequest) {
-        return userService.getAll(userFilterRequest)
-                .thenApply(pair -> {
-                    List<UserResponse> userResponses = pair.getFirst().stream().map(UserResponse::new).toList();
-                    return new PagedListResultResponse<>(userResponses, pair.getSecond());
-                });
+    public PagedListResultResponse<List<UserResponse>> getAllUsers(@Validated UserFilterRequest userFilterRequest) {
+        Pair<List<UserDto>, PageData> dataPair = userService.getAll(userFilterRequest);
+        List<UserResponse> userResponses = dataPair.getFirst().stream().map(UserResponse::new).toList();
+        return new PagedListResultResponse<>(userResponses, dataPair.getSecond());
     }
 }
